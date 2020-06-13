8614cfda8d79cc983bfd01cea2517a789c0d23f3
tcbegley <tomcbegley@gmail.com>
2020-06-13 19:59:01 +0100

    Refactor duplicated code

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -52,39 +52,25 @@ exports.createPages = async ({ graphql, actions }) => {
     .filter(filterBy("recruiting"))
     .sort(sortByOrder)
 
-  financePosts.forEach(({ node }, index) => {
-    const previous = index === 0 ? null : financePosts[index - 1].node
-    const next =
-      index === financePosts.length - 1 ? null : financePosts[index + 1].node
+  const createPagesFromPosts = posts => {
+    posts.forEach(({ node }, index) => {
+      const previous = index === 0 ? null : posts[index - 1].node
+      const next = index === posts.length - 1 ? null : posts[index + 1].node
 
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/post.js`),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.fields.slug,
-        next,
-        previous,
-      },
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+          next,
+          previous,
+        },
+      })
     })
-  })
+  }
 
-  recruitingPosts.forEach(({ node }, index) => {
-    const previous = index === 0 ? null : recruitingPosts[index - 1].node
-    const next =
-      index === recruitingPosts.length - 1 ? null : recruitingPosts[index + 1].node
-
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/post.js`),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.fields.slug,
-        next,
-        previous,
-      },
-    })
-  })
+  createPagesFromPosts(financePosts)
+  createPagesFromPosts(recruitingPosts)
 }
