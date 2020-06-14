db7aea21d4d6d06ab722b47ef33a39179ca2829f
tcbegley <tomcbegley@gmail.com>
2020-06-14 17:37:46 +0100

    Add simple table of contents to each post

--- site/src/templates/post.js
+++ site/src/templates/post.js
@@ -3,16 +3,23 @@ import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/layout"
 import Navigation from "../components/navigation"
+import TOC from "../components/toc"
 
 export default function Post({ data, pageContext }) {
   const post = data.mdx
   const { next, previous } = pageContext
   return (
     <Layout>
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-        <MDXRenderer>{post.body}</MDXRenderer>
-        <Navigation next={next} previous={previous} />
+      <div style={{ display: "grid", gridTemplateColumns: "250px 1fr" }}>
+        <div style={{ gridColumn: 1 }}>
+          <TOC collection={post.fields.collection} />
+        </div>
+        {/* Need to set min width to avoid grid being expanded */}
+        <div style={{ gridColumn: 2, minWidth: 0 }}>
+          <h1>{post.frontmatter.title}</h1>
+          <MDXRenderer>{post.body}</MDXRenderer>
+          <Navigation next={next} previous={previous} />
+        </div>
       </div>
     </Layout>
   )
