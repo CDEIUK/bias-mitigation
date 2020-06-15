a18358e214272c7f0374feee9604e35d4a68b2dc
Gergely Imreh <gergely.imreh@faculty.ai>
2020-06-15 15:05:50 +0100

    update favicon
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- src/site/gatsby-config.js
+++ src/site/gatsby-config.js
@@ -31,13 +31,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `cdei-bias`,
+        short_name: `cdei`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/site-favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
