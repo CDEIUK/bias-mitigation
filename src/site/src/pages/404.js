93e6ea23a5367ae33d1bb900fbd6b6b86ede1890
Gergely Imreh <gergely.imreh@faculty.ai>
2020-06-19 18:23:47 +0100

    adjust the 404 template slightly
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- src/site/src/pages/404.js
+++ src/site/src/pages/404.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { MdErrorOutline } from "react-icons/md"
 
 import Layout from "@rocketseat/gatsby-theme-docs/src/components/Layout"
 import SEO from "@rocketseat/gatsby-theme-docs/src/components/SEO"
@@ -8,10 +9,15 @@ export default function NotFound() {
   return (
     <Layout title="Page not found!">
       <SEO title="404: Not found" />
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>You just hit a route that doesn&#39;t exist...</p>
       <p>
-        If you&#39;d like to go back to homepage, <Link to="/">click here</Link>
-        .
+        If you&#39;d like to go back to start page,{" "}
+        <Link to="/">click here</Link>.
+      </p>
+      <p>
+        <div>
+          <MdErrorOutline size={45} />
+        </div>
       </p>
     </Layout>
   )
