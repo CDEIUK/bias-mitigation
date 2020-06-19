5306748f9b5dbcb62031768b72b4e974491b8eb7
Gergely Imreh <gergely.imreh@faculty.ai>
2020-06-19 16:44:13 +0100

    Linting the pulled in-code
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- src/site/src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/site/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -1,10 +1,10 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled"
 
-import logo from '../assets/logo.svg';
+import logo from "../assets/logo.svg"
 
 export default styled.div`
   width: 160px;
   height: 74px;
   background-size: contain;
   background: url(${logo}) center no-repeat;
-`;
+`
