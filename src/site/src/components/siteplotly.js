8ed07468e7852b695b6bdd2d4d8704d115efbf6d
Gergely Imreh <gergely.imreh@faculty.ai>
2020-06-23 15:57:51 +0100

    more styling to hide narrow plots and enabling formatting of CSS files
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- src/site/src/components/siteplotly.js
+++ src/site/src/components/siteplotly.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Loadable from "react-loadable"
 import Loader from "react-loader-spinner"
+import { MdAspectRatio } from "react-icons/md"
 
 const spinnerStyle = { textAlign: "center" }
 
@@ -24,24 +25,30 @@ const Plotly = Loadable({
 })
 
 export const LazyPlot = ({ ...rest }) => (
-  <Plotly
-    layout={{
-      margin: { t: 0, r: 0, l: 35 },
-      paper_bgcolor: `rgba(0, 0, 0, 0)`,
-      plot_bgcolor: `rgba(0, 0, 0, 0)`,
-      font: {
-        color: `black`,
-        size: 16,
-      },
-      // The next 3 directives make the plot responsive.
-      autosize: true,
-    }}
-    style={{ width: `100%` }}
-    useResizeHandler
-    config={{
-      displayModeBar: false,
-      showTips: false,
-    }}
-    {...rest}
-  />
+  <div>
+    <Plotly
+      layout={{
+        margin: { t: 0, r: 0, l: 35 },
+        paper_bgcolor: `rgba(0, 0, 0, 0)`,
+        plot_bgcolor: `rgba(0, 0, 0, 0)`,
+        font: {
+          color: `black`,
+          size: 16,
+        },
+        // The next 3 directives make the plot responsive.
+        autosize: true,
+      }}
+      style={{ width: `100%` }}
+      useResizeHandler
+      config={{
+        displayModeBar: false,
+        showTips: false,
+      }}
+      {...rest}
+    />
+    <div class="narrow-screen-warning">
+      <MdAspectRatio /> Your screen is too narrow to display a plot. Please try
+      a bigger screen, or landscape mode.
+    </div>
+  </div>
 )
