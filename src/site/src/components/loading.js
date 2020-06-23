c27740a8d4bad3d72c2f8e0839aab2a3c960c2fe
Gergely Imreh <gergely.imreh@faculty.ai>
2020-06-23 13:15:41 +0100

    break out loader into its own component
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

---
+++ src/site/src/components/loading.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Loader from "react-loader-spinner"
+
+const spinnerStyle = { textAlign: "center" }
+
+export default function Loading() {
+  return (
+    <div style={spinnerStyle}>
+      <Loader
+        type="ThreeDots"
+        color="#525453"
+        height={75}
+        width={75}
+        timeout={10000} // 10sec
+      />
+    </div>
+  )
+}
