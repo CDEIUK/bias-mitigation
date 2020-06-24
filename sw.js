7518a8054cc1bba80e810bedd860b71d3bdd9262
imrehg <imrehg@users.noreply.github.com>
2020-06-24 11:32:45 +0000

    deploy: 23059f44e775090a386bf904fdb16c4ed2903959

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3aaccf7938d7e32c7786.js"
+    "url": "webpack-runtime-20999c14be95e6446680.js"
   },
   {
     "url": "framework-290a0347a76cdbe3fd5a.js"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "52731f342ef32ac68daf2151abbe25f2"
+    "revision": "47f0d826e1d03061ff197e4d3e13dc38"
   },
   {
     "url": "pace.min.js"
@@ -100,7 +100,7 @@ self.__precacheManifest = [
     "url": "figure20-969119d19976628b8cd7.js"
   },
   {
-    "url": "figure21-5a6254c972f91e6b99a1.js"
+    "url": "figure21-d5e144eee2cf816c8ddb.js"
   },
   {
     "url": "figure22-248fc17a02dd9aa6bf3c.js"
@@ -124,7 +124,7 @@ self.__precacheManifest = [
     "url": "figure28-74e3c0916f2dcf4509be.js"
   },
   {
-    "url": "figure29-a72d7df37b740ef64c7b.js"
+    "url": "figure29-a97e85ee7c03bad5ccff.js"
   },
   {
     "url": "figure3-59661c515f149dbf6b7e.js"
@@ -217,7 +217,7 @@ self.__precacheManifest = [
     "url": "figure56-0de68576e6c24e443d50.js"
   },
   {
-    "url": "figure57-38918f2c53fbeeb5c361.js"
+    "url": "figure57-28f7f3e2f088b74b4f81.js"
   },
   {
     "url": "figure58-dd675e9a7cb2115e4f12.js"
@@ -241,10 +241,10 @@ self.__precacheManifest = [
     "url": "figure63-dc18fa4f35119b364c34.js"
   },
   {
-    "url": "figure64-6d2be587d5276b50b0f8.js"
+    "url": "figure64-81a971452f47f032f7e7.js"
   },
   {
-    "url": "figure65-fdb942849007968fb389.js"
+    "url": "figure65-d00ef7a2984cc338f426.js"
   },
   {
     "url": "figure66-857749299406f811838f.js"
@@ -267,7 +267,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "d3e633d7f4832d5c71f889fdcc549e24"
+    "revision": "325571625c3fbbbbab596c4babdc0fbb"
   },
   {
     "url": "manifest.webmanifest",
