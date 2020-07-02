da74d08ae56c8ff3da06208f3167e7de89bae2fc
Gergely Imreh <gergely.imreh@faculty.ai>
2020-07-02 20:30:48 +0100

    colour changes for pop
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- src/site/src/@rocketseat/gatsby-theme-docs/styles/theme.js
+++ src/site/src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -1,14 +1,14 @@
 export default {
   colors: {
     primary: "#00a886",
-    background: "#fff",
+    background: "#f0ede3",
     font: "#262445",
     sidebar: {
       background: "",
       link: "#262445",
       heading: "#aaa",
       linkActive: "#13131A",
-      itemActive: "#70cfcf",
+      itemActive: "#f2603b",
       footer: "#A8A8B3",
     },
   },
