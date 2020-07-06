1c36450c98b84e3e816690cd6944c007b1f31745
Gergely Imreh <gergely.imreh@faculty.ai>
2020-07-06 21:35:14 +0100

    format code shadowed from theme
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- src/site/src/@rocketseat/gatsby-theme-docs/components/Header.js
+++ src/site/src/@rocketseat/gatsby-theme-docs/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Headroom from 'react-headroom';
-import styled from '@emotion/styled';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { useStaticQuery, graphql } from 'gatsby';
+import React from "react"
+import PropTypes from "prop-types"
+import Headroom from "react-headroom"
+import styled from "@emotion/styled"
+import { GiHamburgerMenu } from "react-icons/gi"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Container = styled.header`
   display: flex;
@@ -18,7 +18,7 @@ const Container = styled.header`
   transition: transform 0.5s;
 
   transform: translate3d(
-    ${({ isMenuOpen }) => (isMenuOpen ? '240px' : '0')},
+    ${({ isMenuOpen }) => (isMenuOpen ? "240px" : "0")},
     0,
     0
   );
@@ -41,7 +41,7 @@ const Container = styled.header`
   @media (min-width: 780px) {
     display: none;
   }
-`;
+`
 
 export default function Header({ handleMenuOpen, isMenuOpen }) {
   const { site } = useStaticQuery(
@@ -53,10 +53,10 @@ export default function Header({ handleMenuOpen, isMenuOpen }) {
           }
         }
       }
-    `,
-  );
+    `
+  )
 
-  const { siteTitleShort } = site.siteMetadata;
+  const { siteTitleShort } = site.siteMetadata
 
   return (
     <Headroom>
@@ -66,17 +66,17 @@ export default function Header({ handleMenuOpen, isMenuOpen }) {
           type="button"
           onClick={handleMenuOpen}
         >
-          <div style={{color: 'white', background: '#262445'}}>
+          <div style={{ color: "white", background: "#262445" }}>
             <GiHamburgerMenu size={23} />
           </div>
         </button>
         <h2>{siteTitleShort}</h2>
       </Container>
     </Headroom>
-  );
+  )
 }
 
 Header.propTypes = {
   handleMenuOpen: PropTypes.func.isRequired,
   isMenuOpen: PropTypes.bool.isRequired,
-};
+}
